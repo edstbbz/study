568e7986097f1593b8ffd927c2af1d01bbb3ac9a
edstbbz <ed_stubbz@mail.ru>
2021-01-15 04:29:03 +0400

    fp2 test passes

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,41 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        i = 0
+        array = self
+        while i < array.length
+          yield array[i]
+          i += 1
+        end
+        array
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        my_reduce(MyArray.new) do |acc, ele|
+          acc << yield(ele)
+        end
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        my_reduce(MyArray.new) do |acc, ele|
+          ele.nil? ? acc : acc << ele
+        end
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil)
+        my_each do |array|
+          if acc.nil?
+            acc = array
+            next
+          end
+          acc = yield(acc, array)
+        end
+        acc
+      end
     end
   end
 end
